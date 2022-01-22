4afd045b04743162b3128c35ee10418192711f76
anickakm <anna.kmjecova@seznam.cz>
2022-01-22 11:41:34 +0100

    2 files modified

--- ukol1.js
+++ ukol1.js
@@ -1,1 +1,6 @@
-console.log('ahoj');
+let jmeno = 'Petra';
+let vek = 34;
+let vaha = 65;
+let vyska = 1.67;
+let BMI = vaha / (vyska**2);
+console.log('Jmenuji se ' + jmeno + ', je mi ' + vek + ' let a moje BMI je ' + BMI + '.');
